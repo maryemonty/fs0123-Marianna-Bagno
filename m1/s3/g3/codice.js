e36e6c797b231f20b932c0bd3a6985e501151402
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-02-15 17:17:17 +0100

    evvai

--- m1/s3/g3/codice.js
+++ m1/s3/g3/codice.js
@@ -17,29 +17,16 @@ function btn(){
     btn.className = "close";
     btn.appendChild(txt);
     li.appendChild(btn);
-
-    let lista = document.getElementsByTagName("li")
-    let i;
-    for(i= 0; i < lista.length; i++){
-        let btn = document.createElement("button");
-        let txt = document.createTextNode("x");
-        btn.className = "close";
-        btn.appendChild(txt);
-        lista[i].appendChild(btn);
-        let close = document.getElementsByClassName("close");
-        for(i=0; i < close.length; i++){
-            close[i].onclick = function(){
-                let cl = this.parentElement;
-                cl.style.display = "none";
-            }
-        }
+    btn.onclick = function(){
+        li.remove();
+    }
     }
 
-// CHECK
+/*// CHECK
     let check = document.querySelector("ul");
     check.addEventListener('click', function(ev){
         if(ev.target.nodeName == "li"){
             ev.target.classList.toggle('checked');
         }
     })
-}
+} */
